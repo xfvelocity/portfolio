7e619b08823dd342c7aa7bb20d097958c7bca36b
Alex Long <alexlong2001@outlook.com>
2021-10-03 17:40:21 +0100

    mobile test

--- src/pages/about/About.vue
+++ src/pages/about/About.vue
@@ -1,50 +1,68 @@
 <template>
-  <div id="about-page" class="d-flex align-center">
-    <div class="text px-6">
-      <h2 class="mb-6 text-center">About Me</h2>
-      <p>
-        I'm, a self-taught developer with over
-        <span class="light-blue--text text--lighten-2">2 years</span> experience
-        who has a love for web development which is fueled by creating and
-        troubleshooting code. It started with coding being a hobby which has now
-        led to a full-time career.
-      </p>
-      <p class="mt-8">
-        Experience with a range of Javascript frameworks such as
-        <span class="light-blue--text text--lighten-2">Vue</span> &
-        <span class="light-blue--text text--lighten-2">Svelte</span>. Paired
-        alongside with
-        <span class="light-blue--text text--lighten-2">Vuetify</span> &
-        <span class="light-blue--text text--lighten-2">Tailwind</span> to build
-        stunning looking websites. I can also build backend systems using
-        <span class="light-blue--text text--lighten-2">NodeJS</span> &
-        <span class="light-blue--text text--lighten-2">MongoDB</span> to help
-        support these.
-      </p>
+  <v-container id="about-page" class="d-flex align-center px-6">
+    <v-row>
+      <v-col cols="12">
+        <img
+          style="width: 200px; display: block; margin: 0 auto"
+          src="@/assets/solar-system.svg"
+          alt=""
+        />
+      </v-col>
+      <v-col class="text" cols="12">
+        <h2 class="mb-4 text-center">About Me</h2>
+        <p>
+          I'm, a self-taught developer with over
+          <span class="light-blue--text text--lighten-2">2 years</span>
+          experience who has a love for web development which is fueled by
+          creating and troubleshooting code. It started with coding being a
+          hobby which has now led to a full-time career.
+        </p>
+        <p>
+          Experience with a range of Javascript frameworks such as
+          <span class="light-blue--text text--lighten-2">Vue</span> &
+          <span class="light-blue--text text--lighten-2">Svelte</span>. Paired
+          alongside with
+          <span class="light-blue--text text--lighten-2">Vuetify</span> &
+          <span class="light-blue--text text--lighten-2">Tailwind</span> to
+          build stunning looking websites. I can also build backend systems
+          using <span class="light-blue--text text--lighten-2">NodeJS</span> &
+          <span class="light-blue--text text--lighten-2">MongoDB</span> to help
+          support these.
+        </p>
 
-      <div class="mt-8">
-        <a
-          class="white--text font-weight-medium text-decoration-none mr-1"
-          href="/alexlong-cv.pdf"
+        <div
+          class="d-flex mt-8"
+          @click="goToLink('/src/assets/alexlong-cv.pdf')"
         >
-          CV / Resume
-        </a>
-        <v-icon color="white">file_download</v-icon>
-      </div>
-    </div>
-  </div>
+          <p class="font-weight-medium mr-1 mb-0">CV / Resume</p>
+          <v-icon color="white">file_download</v-icon>
+        </div>
+      </v-col>
+      <!-- <v-col cols="12">
+        <img style="width: 100%" src="@/assets/solar-system.svg" alt="" />
+      </v-col> -->
+    </v-row>
+  </v-container>
 </template>
  
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class About extends Vue {}
+export default class About extends Vue {
+  goToLink(link: string): void {
+    window.open(link);
+  }
+}
 </script>
 <style lang="scss">
 #about-page {
   height: 80vh;
+  margin: 0 auto;
+
   .text {
+    max-width: 450px;
+    margin: 0 auto;
     h2 {
       font-size: 44px;
     }
