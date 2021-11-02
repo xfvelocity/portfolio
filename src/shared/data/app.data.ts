23419a425937ea0a369e6df54ee8af479650e3b8
Alex Long <alexlong2001@outlook.com>
2021-11-02 07:33:52 +0000

    fixed wording

--- src/shared/data/app.data.ts
+++ src/shared/data/app.data.ts
@@ -10,7 +10,7 @@ export const pages: Page[] = [
     info: {
       name: "Build A-Dream",
       desc:
-        "This was one of my first projects that I started working on and its been re-worked multiple times, currently using vue & nuxtjs. This is a website to help promote a family members landscaping business.",
+        "This was one of the first projects that I started working on and it's been re-worked multiple times, currently using Vue & Nuxtjs. This website was built for a family member to help promote their landscaping business.",
       github: "https://github.com/xfvelocity/build-a-dream",
       link: "https://buildadream.co.uk/",
       technologies: ["typescript", "vue", "vuetify", "nuxt"],
@@ -24,7 +24,7 @@ export const pages: Page[] = [
     info: {
       name: "Hosting Solutions",
       desc:
-        "HostingSolutions was a website in which I wanted to challenge my base html/css skills. This is one of my favourite websites i’ve created and tought me a lot about svg animations. v3",
+        "HostingSolutions was a website in which I wanted to challenge my base HTML/CSS skills. This is one of the favourite websites I’ve created as it has taught me a lot about SVG animations.",
       github: "https://github.com/xfvelocity/hosting-solutions",
       link: "",
       technologies: ["html", "css", "javascript", "sass"],
@@ -38,7 +38,7 @@ export const pages: Page[] = [
     info: {
       name: "Gym Tracker",
       desc:
-        " I wanted to try my hand at a full-stack project which I could use everyday so I created the gym tracker, orignally used for meal prep it’s developed into a seamless gym tracking program",
+        "I wanted to try my hand at a full-stack project which I could use every day so I created the gym tracker, originally used for meal prep it’s developed into a seamless gym tracking app",
       github: "https://github.com/xfvelocity/gym-progress-tracker",
       link: "https://www.gymprogresstracker.com",
       technologies: ["typescript", "vue", "vuetify", "mongodb", "nodejs"],
