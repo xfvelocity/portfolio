84eed0be9ab58de3888302897808cab7052a587b
Alex Long <alexlong2001@outlook.com>
2023-06-17 13:37:05 +0100

    trying scroll snap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="slide-fade-top" appear>
+  <!-- <transition name="slide-fade-top" appear>
     <nav class="nav xf-flex xf-p-4">
       <a
         class="xf-hover xf-ml-auto"
@@ -16,7 +16,14 @@
     <About />
     <Projects v-for="(project, i) in projects" :key="i" :info="project" />
     <Contact />
-  </main>
+  </main> -->
+
+  <div class="container">
+    <Header />
+    <About />
+    <Projects v-for="(project, i) in projects" :key="i" :info="project" />
+    <Contact />
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -31,7 +38,47 @@ import Projects from "@/sections/projects/Projects.vue";
 import Contact from "@/sections/contact/Contact.vue";
 
 // ** Lifecycle **
-onMounted(useMediaQuery);
+onMounted(() => {
+  useMediaQuery();
+
+  // const sections = document.querySelectorAll(".section");
+  // let currentSectionIndex = 0;
+  // let isScrolling = false;
+
+  // const scrollToSection = (index: number): void => {
+  //   isScrolling = true;
+
+  //   sections[currentSectionIndex].classList.remove("active");
+  //   sections[index].classList.add("active");
+
+  //   window.scrollTo({
+  //     top: sections[index].offsetTop,
+  //     behavior: "smooth",
+  //   });
+
+  //   setTimeout(() => {
+  //     isScrolling = false;
+  //     currentSectionIndex = index;
+  //   }, 1000);
+  // };
+
+  // const handleScroll = (event: MouseEvent): void => {
+  //   if (isScrolling) return;
+
+  //   const direction = event.wheelDelta > 0 ? "up" : "down";
+
+  //   if (direction === "up" && currentSectionIndex > 0) {
+  //     scrollToSection(currentSectionIndex - 1);
+  //   } else if (
+  //     direction === "down" &&
+  //     currentSectionIndex < sections.length - 1
+  //   ) {
+  //     scrollToSection(currentSectionIndex + 1);
+  //   }
+  // };
+
+  // document.addEventListener("mousewheel", handleScroll);
+});
 </script>
 
 <style lang="scss">
@@ -42,5 +89,20 @@ onMounted(useMediaQuery);
   position: absolute;
   width: 100%;
   z-index: 99;
+}
+
+html,
+body {
+  margin: 0 !important;
+  padding: 0 !important;
+  height: 100% !important;
+  overflow: hidden !important;
+  scroll-behavior: smooth;
+}
+
+.container {
+  height: 100%;
+  scroll-snap-type: y mandatory;
+  overflow-y: scroll;
 }
 </style>
